3ebdc741b43925e330d499ee1c1247a4807e5f70
Adele2007 <adelyaalmazova@icloud.com>
2022-09-29 16:54:22 +0600

    Add code using boolean algebra

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        ш
+
+        int age = 35;
+        int temperature = 28;
+
+        if (age > 20 - 45 || temperature > 20 - 30) {
+            System.out.println("Можно идти гулять!");
+        } else if (age < 20 || temperature < 0 - 28) {
+            System.out.println("Можно идти гулять!");
+        } else if (age > 45 || temperature > 10 - 25) {
+            System.out.println("Можно идти гулять!");
+        } else {
+            System.out.println("Оставайтесь дома!");
+        }
+
+    }
+}
